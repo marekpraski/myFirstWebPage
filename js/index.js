1d052d0a8b145034b72598f41291a2ef6aa82191
Marek Praski <marekpraski@prgw.com.pl>
2022-12-22 21:13:14 +0100

    prosty kalkulator na dwóch losowych liczbach, wyświetla wynik w zależności od klikniętego przycisku

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
-var btns = document.querySelectorAll(".drum");
-for(var i = 0; btns.length; i++){
-    btns[i].addEventListener("click", onClick);
+
+function getRandom(){
+    return Math.floor(Math.random() * 100);
 }
 
-function onClick(){
-    alert("I got clicked");
+function add(x, y){
+    return x + y;
 }
+function subtract(x, y){
+    return x-y;
+}
+function multiply(x, y){
+    return x * y;
+}
+function divide(x, y){
+    return x/y;
+}
+function getResult(x, y, operator){
+    document.querySelector(".result").textContent =  operator(x, y);
+}
+
+document.querySelector(".add").addEventListener("click", function(){getResult(x, y, add)});
+document.querySelector(".multiply").addEventListener("click", function(){getResult(x, y, multiply)});
+document.querySelector(".subtract").addEventListener("click", function(){getResult(x, y, subtract)});
+document.querySelector(".divide").addEventListener("click", function(){getResult(x, y, divide)});
+
+document.querySelector(".random1").textContent = getRandom();
+document.querySelector(".random2").textContent = getRandom();
+var x = Number(document.querySelector(".random1").textContent);
+var y = Number(document.querySelector(".random2").textContent);
