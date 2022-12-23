de203f77c4c4de69e7e9346e12e28ff778b70a9f
Marek Praski <marekpraski@prgw.com.pl>
2022-12-23 08:54:32 +0100

    do przycisków przypisałem obrazki; po naciśnięciu przycisku odtwarzany jest dźwięk (tai sam dla każdego przycisku)

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 var btns = document.querySelectorAll(".drum");
+
 for(var i = 0; btns.length; i++){
     btns[i].addEventListener("click", onClick);
 }
 
+
 function onClick(){
-    alert("I got clicked");
+    var sound = new Audio("sounds/tom-1.mp3");
+    sound.play();
 }
