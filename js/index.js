de1006eadb664d16f58192a3c01cd11d560989cf
Marek Praski <marekpraski@prgw.com.pl>
2022-12-27 20:57:45 +0100

    kliknięcie myszą na przycisk odtwarza odpowiedni dźwięk (tworzenie obiektów oraz składnia switch)

--- js/index.js
+++ js/index.js
@@ -1,10 +1,47 @@
 var btns = document.querySelectorAll(".drum");
+var keyW = new Key("sounds/tom-1.mp3");
+var keyA = new Key("sounds/tom-2.mp3");
+var keyS = new Key("sounds/tom-3.mp3");
+var keyD = new Key("sounds/tom-4.mp3");
+var keyJ = new Key("sounds/crash.mp3");
+var keyK = new Key("sounds/kick-bass.mp3");
+var keyL = new Key("sounds/snare.mp3");
 
 for(var i = 0; btns.length; i++){
     btns[i].addEventListener("click", onClick);
 }
 
+function Key (music){
+    this.music = music;
+    this.play = function(){
+        var a = new Audio(music);
+        a.play();
+    }
+}
+
 function onClick(){
-    var txt = this.innerText;
-    document.querySelector("." + txt).classList.add("clicked");
+    var t = this.innerText;
+    switch (t) {
+        case "w":
+            keyW.play();
+            break;
+        case "a":
+            keyA.play();
+            break;
+        case "s":
+            keyS.play();
+            break;
+        case "d":
+            keyD.play();
+            break;
+        case "j":
+            keyJ.play();
+            break;
+        case "k":
+            keyK.play();
+            break;
+        case "l":
+            keyL.play();
+            break;
+    }
 }
