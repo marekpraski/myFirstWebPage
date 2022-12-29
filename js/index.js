2fc7b0e642981b28a22143a18c1f47f6255a93f8
Marek Praski <marekpraski@prgw.com.pl>
2022-12-29 18:32:46 +0100

    do przycisków przypisałem nasłuch na kliknięcie myszką; po kliknięciu miga losowy przycisk i odtwarzany jest odpowiadający temu przyciskowi dźwięk

--- js/index.js
+++ js/index.js
@@ -1,65 +1,32 @@
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-var btns = document.querySelectorAll(".drum");
-var keyW = new Key("sounds/tom-1.mp3");
-var keyA = new Key("sounds/tom-2.mp3");
-var keyS = new Key("sounds/tom-3.mp3");
-var keyD = new Key("sounds/tom-4.mp3");
-var keyJ = new Key("sounds/crash.mp3");
-var keyK = new Key("sounds/kick-bass.mp3");
-var keyL = new Key("sounds/snare.mp3");
+var randomNumber = nextSequence();
+var randomChosenColour = buttonColours[randomNumber];
+gamePattern.push(randomChosenColour);
+var currentButton = document.querySelector("." + randomChosenColour);
 
-function Key (music){
-    this.music = music;
-    this.play = function(){
-        var a = new Audio(music);
-        a.play();
-    }
+var allButtons = document.querySelectorAll(".btn");
+
+for(var i = 0; i < allButtons.length; i++){
+    allButtons[i].addEventListener("click", whenClicked);
 }
 
-document.addEventListener("keydown", onKeyPressed);
-
-for(var i = 0; i < btns.length; i++){
-    btns[i].addEventListener("click", onClick);
+function nextSequence(){
+    return Math.floor(Math.random() * 3) + 1;
 }
 
-function onKeyPressed(eventArgs){
-    playMe(eventArgs.key);
+function flashButton(button){
+    button.classList.add("pressed");
+    setTimeout(function(){button.classList.remove("pressed");}, 200);
 }
 
-function animateMe(keyCode){
-    var kp = document.querySelector("." + keyCode);
-    kp.classList.add("pressed");
-    setTimeout(function(){kp.classList.remove("pressed")}, 200);
+function playSound(button){
+    var sound = new Audio("sounds/" + button + ".mp3");
+    sound.play();
 }
 
-function onClick(){
-    playMe(this.innerText);
+function whenClicked(){
+    flashButton(currentButton);
+    playSound(randomChosenColour);
 }
-
-function playMe(code){
-    animateMe(code);
-    switch (code) {
-        case "w":
-            keyW.play();
-            break;
-        case "a":
-            keyA.play();
-            break;
-        case "s":
-            keyS.play();
-            break;
-        case "d":
-            keyD.play();
-            break;
-        case "j":
-            keyJ.play();
-            break;
-        case "k":
-            keyK.play();
-            break;
-        case "l":
-            keyL.play();
-            break;
-    }
-}
-
