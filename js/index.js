aeadedc53c14b769036f8fdc5f71fd1636a0841e
Marek Praski <marekpraski@prgw.com.pl>
2023-01-02 12:14:35 +0100

    działająca gra

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 var gamePattern = [];
 var userClickedPattern = [];
+var isGameStarted = false;
+var level = 0;
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomNumber = nextSequence();
-var randomChosenColour = buttonColours[randomNumber];
-gamePattern.push(randomChosenColour);
-var randomChosenButton = document.querySelector("." + randomChosenColour);
+var randomNumber;
+var randomChosenColour;
+var randomChosenButton;
 
 var allButtons = document.querySelectorAll(".btn");
 
@@ -13,14 +14,49 @@ for(var i = 0; i < allButtons.length; i++){
     allButtons[i].addEventListener("click", whenClicked);
 }
 
-document.addEventListener("keydown", whenKeyPressed);
+document.addEventListener("keydown", startGame);
 
-function whenKeyPressed(){
-    flashButton(randomChosenButton);
+function startGame(){
+    if(!isGameStarted)
+        nextSequence();
+    isGameStarted = true; 
 }
 
-
 function nextSequence(){
+    level++;
+    userClickedPattern = [];
+    document.querySelector("h1").innerText = ("Level " + level);
+    randomNumber = generateRandomNumber();
+    randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    randomChosenButton = document.querySelector("." + randomChosenColour);
+    flashButton(randomChosenButton);   
+}
+
+function checkAnswer(index){
+    if(gamePattern[index] !== userClickedPattern[index])
+        gameOver();
+    else if(index === gamePattern.length - 1)
+        setTimeout(nextSequence,500);
+}
+
+function gameOver(){
+    gamePattern = [];
+    userClickedPattern = [];
+    isGameStarted = false;
+    level = 0;
+    document.querySelector("h1").innerText = "Game over, Press A Key to Restart";
+}
+
+function whenClicked(){
+    var btnClicked = this.id;
+    userClickedPattern.push(btnClicked);
+    playSound(btnClicked);
+    flashButton(this);
+    checkAnswer(userClickedPattern.length - 1);
+}
+
+function generateRandomNumber(){
     return Math.floor(Math.random() * 3) + 1;
 }
 
@@ -32,11 +68,4 @@ function flashButton(button){
 function playSound(button){
     var sound = new Audio("sounds/" + button + ".mp3");
     sound.play();
-}
-
-function whenClicked(){
-    var btnClicked = this.id;
-    userClickedPattern.push(btnClicked);
-    playSound(btnClicked);
-    flashButton(this);
 }
